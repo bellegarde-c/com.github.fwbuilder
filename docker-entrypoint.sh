62eb279b1bb75e0e2eea75cc77f1fb00086ed621
Henri Wahl <h.wahl@ifw-dresden.de>
2021-01-05 18:51:02 +0100

    made it at least running - might need some finetuning

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ flatpak install --assumeyes org.kde.Platform/x86_64/5.14 \
 
 cd /build
 
-flatpak-builder fp_build --force-clean com.github.fwbuilder.json
+flatpak-builder --repo=fwbuilder_repo fwbuilder_build --force-clean com.github.fwbuilder.json
